8d21b00e7a5720d80f7505d4a9e2117d0f358cdc
samuelralak <samuelralak@hotmail.com>
2016-06-15 15:38:33 +0300

    making the app heroku ready

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :novel, Novel.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "adrite.herokuapp.com", port: 443], force_ssl: [rewrite_on:[:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -60,6 +61,13 @@ config :logger, level: :info
 #
 #     config :novel, Novel.Endpoint, root: "."
 
+# Configure your database
+config :novel, Novel.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
