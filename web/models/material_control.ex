b18cf08e38a3ed4e2897df2288a263339688cea6
samuelralak <samuelralak@hotmail.com>
2016-07-08 17:03:05 +0300

    bug fixes

--- web/models/material_control.ex
+++ web/models/material_control.ex
@@ -47,7 +47,7 @@ defmodule Novel.MaterialControl do
   			|> put_change(:control_id, control.id)
   		control ->
   			if total_cost = get_change(changeset, :total_cost) do
-  				old_cost = control.total_cost - default_for_nil(changeset.model.total_cost)
+  				old_cost = default_for_nil(control.total_cost) - default_for_nil(changeset.model.total_cost)
   				new_cost = default_for_nil(old_cost) + default_for_nil(total_cost)
   				control = Ecto.Changeset.change(control, total_cost: new_cost) 
   				Novel.Repo.update control
